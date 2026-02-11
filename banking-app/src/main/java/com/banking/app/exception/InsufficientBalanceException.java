ed5129ac6cca7e3bcc29851e9a1e8f477138989d
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-11 17:53:11 -0600

    InsufficientBalanceException.java

--- banking-app/src/main/java/com/banking/app/exception/InsufficientBalanceException.java
+++ banking-app/src/main/java/com/banking/app/exception/InsufficientBalanceException.java
@@ -1,4 +1,12 @@
 package com.banking.app.exception;
 
-public interface InsufficientBalanceException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
 }
