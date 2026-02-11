9706dab542c66b4880ee88b07d655753bbdeb03e
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-11 17:53:43 -0600

    Modified AccountNotFoundException.java

--- banking-app/src/main/java/com/banking/app/exception/AccountNotFoundException.java
+++ banking-app/src/main/java/com/banking/app/exception/AccountNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public interface AccountNotFoundException extends RuntimeException {
-    public class AccountNotFoundException (String message){
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException (String message){
         super(message);
     }
 }
