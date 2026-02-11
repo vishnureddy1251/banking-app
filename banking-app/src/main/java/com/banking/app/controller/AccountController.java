55d714dc89e51687c4402ac4830b64acf594712c
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-10 21:51:56 -0600

    Updated AccountController

--- banking-app/src/main/java/com/banking/app/controller/AccountController.java
+++ banking-app/src/main/java/com/banking/app/controller/AccountController.java
@@ -5,11 +5,12 @@ import com.banking.app.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,5 +35,25 @@ public class AccountController {
     public ResponseEntity<List<Account>> getAllAccounts() {
         List<Account> accounts = accountService.getAllAccounts();
         return ResponseEntity.ok(accounts);
+    }
+
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<Account> deposit(
+            @PathVariable Long id,
+            @RequestBody Map<String, BigDecimal> request) {
+
+        BigDecimal amount = request.get("amount");
+        Account updated = accountService.deposit(id, amount);
+        return ResponseEntity.ok(updated);
+    }
+
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<Account> withdraw(
+            @PathVariable Long id,
+            @RequestBody Map<String, BigDecimal> request) {
+
+        BigDecimal amount = request.get("amount");
+        Account updated = accountService.withdraw(id, amount);
+        return ResponseEntity.ok(updated);
     }
 }
