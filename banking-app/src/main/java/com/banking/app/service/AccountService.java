883da61d7e76ea2259cea4eed1c11f5a95c99839
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-10 21:02:36 -0600

    Modified AccountService.java

--- banking-app/src/main/java/com/banking/app/service/AccountService.java
+++ banking-app/src/main/java/com/banking/app/service/AccountService.java
@@ -1,18 +1,23 @@
 package com.banking.app.service;
 
 import com.banking.app.model.Account;
+import com.banking.app.repository.AccountRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AccountService {
+
+    private final AccountRepository accountRepository;
 
     @Transactional
     public Account createAccount(Account account){
@@ -26,6 +31,29 @@ public class AccountService {
         Account savedAccount = accountRepository.save(account);
         log.info("Account created: {} for {}", savedAccount.getAccountNumber(), savedAccount.getAccountHolderName());
         return savedAccount;
+    }
 
+    public Account getAccountById(Long id){
+        return accountRepository.findById(id)
+                .orElseThrow(() -> new AccountNotFoundException("Account not found with ID: " +id));
+    }
+
+    public List<Account> getAllAccounts(){
+        return accountRepository.findAll();
+    }
+
+    @Transactional
+    public Account deposit(Long accountId, BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO) <=0){
+            throw new IllegalArgumentException("Deposit Amount must be Positive");
+        }
+
+        Account account = getAccountById(accountId);
+        BigDecimal newBalance = account.getBalance().add(amount);
+        account.setBalance(newBalance);
+
+        Account updated = accountRepository.save(account);
+        log.info("Deposited {} to account {}. New Balance: {}", amount, account.getAccountNumber(), newBalance);
+        return updated;
     }
 }
