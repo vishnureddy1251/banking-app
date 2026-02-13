4c33a7335c25aa9ef5ecfe66e9b2f55253d56602
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-13 16:01:30 -0600

    Modified CustomerService.java

--- banking-app/src/main/java/com/banking/app/service/CustomerService.java
+++ banking-app/src/main/java/com/banking/app/service/CustomerService.java
@@ -1,5 +1,8 @@
 package com.banking.app.service;
 
+import com.banking.app.model.Customer;
+import com.banking.app.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Transactional
+    public Customer createCustomer(Customer customer){
+
+        if (customerRepository.existsByEmail(customer.getEmail())){
+            throw new IllegalArgumentException("Email already registered: " + customer.getEmail());
+        }
+        Customer saved = customerRepository.save(customer);
+        log.info("Customer created: {} {}", saved.getFirstName(), saved.getLastName());
+        return saved;
+    }
 }
