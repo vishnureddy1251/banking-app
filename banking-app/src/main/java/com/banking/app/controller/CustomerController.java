975aa2ff8966fdb66f1be8adf2c08469942af470
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-13 16:34:32 -0600

    Modified CustomerController.java

--- banking-app/src/main/java/com/banking/app/controller/CustomerController.java
+++ banking-app/src/main/java/com/banking/app/controller/CustomerController.java
@@ -1,6 +1,13 @@
 package com.banking.app.controller;
 
+import com.banking.app.model.Customer;
+import com.banking.app.service.CustomerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer){
+        Customer created = customerService.createCustomer(customer);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
 }
