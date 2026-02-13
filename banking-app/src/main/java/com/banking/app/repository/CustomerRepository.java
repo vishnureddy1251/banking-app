9a52d4b1999a5b1af4bd7275a183b55bad026f8e
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-12 22:20:04 -0600

    Modified CustomerRepository.java

--- banking-app/src/main/java/com/banking/app/repository/CustomerRepository.java
+++ banking-app/src/main/java/com/banking/app/repository/CustomerRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CustomerRepository extends JpaRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByEmail(String email);
 
