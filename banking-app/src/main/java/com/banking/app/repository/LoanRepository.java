4d5447fdea68e8e01df17f8116659e8c18105714
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-13 22:50:46 -0600

    Added LoanRepository.java

---
+++ banking-app/src/main/java/com/banking/app/repository/LoanRepository.java
@@ -0,0 +1,10 @@
+package com.banking.app.repository;
+
+import com.banking.app.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+}
