9c060581aded47ae96822c61fac6d2dc0b810445
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-14 22:56:51 -0600

    Added TransactionController.java

---
+++ banking-app/src/main/java/com/banking/app/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.banking.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+}
