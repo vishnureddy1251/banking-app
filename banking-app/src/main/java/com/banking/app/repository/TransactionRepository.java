59b90c767be7402ab655f994cb25395f1ae44865
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-14 22:53:46 -0600

    Added TransactionRepository.java

---
+++ banking-app/src/main/java/com/banking/app/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.banking.app.repository;
+
+import com.banking.app.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
