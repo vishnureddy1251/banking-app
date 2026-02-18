db61d85a0b34c3bd17f847fb80ebeb713d170a18
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-17 22:23:06 -0600

    Modified UserRepository.java

--- banking-app/src/main/java/com/banking/app/repository/UserRepository.java
+++ banking-app/src/main/java/com/banking/app/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.banking.app.repository;
 
 import com.banking.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
