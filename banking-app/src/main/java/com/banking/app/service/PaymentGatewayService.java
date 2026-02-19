5bdd8d8ac317905e43752344fbd73a71805e5c10
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-18 22:31:24 -0600

    Modified PaymentGatewayService.java

--- banking-app/src/main/java/com/banking/app/service/PaymentGatewayService.java
+++ banking-app/src/main/java/com/banking/app/service/PaymentGatewayService.java
@@ -3,7 +3,36 @@ package com.banking.app.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Random;
+
 @Service
 @Slf4j
 public class PaymentGatewayService {
+
+    private boolean failureMode = false;
+
+    private final Random random = new Random();
+
+    public Map<String, Object> processPayment(Long accountId, BigDecimal amount, String description){
+
+        log.info("Calling Payment Gateway for account {} - {}", accountId, amount);
+
+        try{
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
+        if (failureMode){
+            log.error("Payment Gateway is DOWN! (failure mode ON)");
+            throw new RuntimeException("Payment Gateway is unavailable - connection timeout");
+        }
+
+    }
+
+    public boolean isFailureModeEnabled(){
+        return this.failureMode;
+    }
 }
