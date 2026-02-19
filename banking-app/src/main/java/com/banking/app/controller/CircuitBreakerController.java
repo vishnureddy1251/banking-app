efe0b264821e9b879960f35e7955369d3a8643da
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-18 22:48:52 -0600

    Modified CircuitBreakerService.java & CircuitBreakerController.java

--- banking-app/src/main/java/com/banking/app/controller/CircuitBreakerController.java
+++ banking-app/src/main/java/com/banking/app/controller/CircuitBreakerController.java
@@ -1,4 +1,11 @@
 package com.banking.app.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/circuit-breaker")
+@RequiredArgsConstructor
 public class CircuitBreakerController {
 }
