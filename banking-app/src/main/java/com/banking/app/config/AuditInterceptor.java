b172d342d03754e33cc771ca4c3f2e4a8d4b018a
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-20 22:55:48 -0600

    Added AuditInterceptor.java

---
+++ banking-app/src/main/java/com/banking/app/config/AuditInterceptor.java
@@ -0,0 +1,11 @@
+package com.banking.app.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AuditInterceptor {
+}
