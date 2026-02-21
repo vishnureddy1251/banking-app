42e78b8e942c9962705a116b4497ca0d0e69bb63
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-20 22:54:07 -0600

    Modified WebConfig.java

--- banking-app/src/main/java/com/banking/app/config/WebConfig.java
+++ banking-app/src/main/java/com/banking/app/config/WebConfig.java
@@ -1,4 +1,9 @@
 package com.banking.app.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class WebConfig {
 }
