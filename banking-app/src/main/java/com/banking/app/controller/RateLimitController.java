55a5b740cdf2b7944e32de426c55da02d966af6e
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-20 22:33:08 -0600

    Added RateLimitController.java

---
+++ banking-app/src/main/java/com/banking/app/controller/RateLimitController.java
@@ -0,0 +1,9 @@
+package com.banking.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/rate-limit")
+public class RateLimitController {
+}
