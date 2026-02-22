86db7e45ddc020c34d15399678f711ea3f14ef2c
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-21 23:16:57 -0600

    Added SecurityConfig.java

---
+++ banking-app/src/main/java/com/banking/app/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.banking.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+}
