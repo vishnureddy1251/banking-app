8cf1194c54f653a80511f283429a637d336a44cb
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-21 23:20:13 -0600

    Modified AuditInterceptor.java

--- banking-app/src/main/java/com/banking/app/config/AuditInterceptor.java
+++ banking-app/src/main/java/com/banking/app/config/AuditInterceptor.java
@@ -1,11 +1,61 @@
 package com.banking.app.config;
 
+import com.banking.app.service.AuditLogService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class AuditInterceptor {
+public class AuditInterceptor implements HandlerInterceptor{
+
+    private final AuditLogService auditLogService;
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception ex) {
+
+        String endpoint = request.getRequestURI();
+
+        if (!endpoint.startsWith("/api/")) {
+            return;
+        }
+
+        String username = "ANONYMOUS";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal())) {
+            username = auth.getName();
+        }
+
+        String httpMethod = request.getMethod();
+        String action = mapHttpMethodToAction(httpMethod);
+
+        String entityType = extractEntityType(endpoint);
+
+        String description = httpMethod + " " + endpoint;
+        if (ex != null) {
+            description += " - ERROR: " + ex.getMessage();
+        }
+
+        String ipAddress = getClientIp(request);
+
+        auditLogService.logAction(
+                username,
+                action,
+                entityType,
+                null,
+                description,
+                httpMethod,
+                endpoint,
+                ipAddress,
+                response.getStatus()
+        );
+    }
+
 }
