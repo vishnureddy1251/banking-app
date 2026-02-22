55efad00a1bbbff5a58041e14a198b70d2e43b13
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-22 14:18:57 -0600

    Added JwtAuthFilter.java

--- banking-app/src/main/java/com/banking/app/security/JwtAuthFilter.java
+++ banking-app/src/main/java/com/banking/app/security/JwtAuthFilter.java
@@ -1,4 +1,12 @@
 package com.banking.app.security;
 
-public class JwtAuthFilter {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
 }
