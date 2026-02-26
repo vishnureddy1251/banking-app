0a27e808163b2701f7d1f1304beb6f085bf7a4cd
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-25 22:06:56 -0600

    Added ResilienceController.java

---
+++ banking-app/src/main/java/com/banking/app/controller/ResilienceController.java
@@ -0,0 +1,18 @@
+package com.banking.app.controller;
+
+import com.banking.app.service.ResilienceService;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.RetryRegistry;
+import lombok.RequiredArgsConstructor;
+
+    @RestController
+    @RequestMapping("api/v1/resilience")
+    @RequiredArgsConstructor
+    public class ResilienceController {
+
+        private final ResilienceService resilienceService;
+        private final CircuitBreakerRegistry circuitBreakerRegistry;
+        private final RetryRegistry retryRegistry;
+        private final BulkheadRegistry bulkheadRegistry;
+    }
