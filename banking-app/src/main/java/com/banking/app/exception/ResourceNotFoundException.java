9620af8e8878b7bf117479bdfe21731ed188499c
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-02-28 22:31:04 -0600

    Modified ResourceNotFoundException.java

--- banking-app/src/main/java/com/banking/app/exception/ResourceNotFoundException.java
+++ banking-app/src/main/java/com/banking/app/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.banking.app.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
