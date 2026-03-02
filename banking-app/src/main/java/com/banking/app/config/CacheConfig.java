b9b9eef0d72371b9ff18b5845e2e089fa3e61b3f
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-01 22:51:24 -0600

    Modified CacheConfig.java

--- banking-app/src/main/java/com/banking/app/config/CacheConfig.java
+++ banking-app/src/main/java/com/banking/app/config/CacheConfig.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.boot.autoconfigure.cache.CacheProperties.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
