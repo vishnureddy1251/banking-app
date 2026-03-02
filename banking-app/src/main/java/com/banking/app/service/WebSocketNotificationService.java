cfc9abf852113fda2eb28ff678d92a9509fbb588
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-02 15:41:48 -0600

    Added WebSocketNotificationService.java

---
+++ banking-app/src/main/java/com/banking/app/service/WebSocketNotificationService.java
@@ -0,0 +1,14 @@
+package com.banking.app.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketNotificationService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+}
