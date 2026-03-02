8ca90bc77d6406d12ef63e8ea5980fa4efaec4b5
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-02 15:47:37 -0600

    Modified WebSocketNotificationService.java

--- banking-app/src/main/java/com/banking/app/service/WebSocketNotificationService.java
+++ banking-app/src/main/java/com/banking/app/service/WebSocketNotificationService.java
@@ -5,10 +5,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketNotificationService {
 
     private final SimpMessagingTemplate messagingTemplate;
+
+    public void notifyDeposit(Long accountId, BigDecimal amount, BigDecimal newBalance) {
+        Map<String, Object> payload = Map.of(
+                "type", "DEPOSIT",
+                "title", "Deposit Received",
+                "message", "You received a deposit of $" + amount,
+                "amount", amount.toString(),
+                "newBalance", newBalance.toString(),
+                "accountId", accountId,
+                "timestamp", LocalDateTime.now().toString()
+        );
+
+        sendToAccount(accountId, payload);
+        log.info("WebSocket: Deposit notification sent to account {}", accountId);
+    }
 }
