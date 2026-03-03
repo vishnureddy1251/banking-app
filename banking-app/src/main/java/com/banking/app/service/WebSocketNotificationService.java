7d5eb45df132c2c04f5b48c17564ca381b720426
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-02 21:21:42 -0600

    Modified WebSocketNotificationService.java

--- banking-app/src/main/java/com/banking/app/service/WebSocketNotificationService.java
+++ banking-app/src/main/java/com/banking/app/service/WebSocketNotificationService.java
@@ -30,4 +30,53 @@ public class WebSocketNotificationService {
         sendToAccount(accountId, payload);
         log.info("WebSocket: Deposit notification sent to account {}", accountId);
     }
+
+    public void notifyWithdrawal(Long accountId, BigDecimal amount, BigDecimal newBalance) {
+        Map<String, Object> payload = Map.of(
+                "type", "WITHDRAWAL",
+                "title", "Withdrawal Made",
+                "message", "You withdrew $" + amount + " from your account",
+                "amount", amount.toString(),
+                "newBalance", newBalance.toString(),
+                "accountId", accountId,
+                "timestamp", LocalDateTime.now().toString()
+        );
+
+        sendToAccount(accountId, payload);
+        log.info("WebSocket: Withdrawal notification sent to account {}", accountId);
+    }
+
+    public void notifyTransferSent(Long fromAccountId, Long toAccountId,
+                                   BigDecimal amount, BigDecimal newBalance) {
+        Map<String, Object> payload = Map.of(
+                "type", "TRANSFER_SENT",
+                "title", "Transfer Sent",
+                "message", "You sent $" + amount + " to account #" + toAccountId,
+                "amount", amount.toString(),
+                "newBalance", newBalance.toString(),
+                "toAccountId", toAccountId,
+                "accountId", fromAccountId,
+                "timestamp", LocalDateTime.now().toString()
+        );
+
+        sendToAccount(fromAccountId, payload);
+        log.info("WebSocket: Transfer sent notification to account {}", fromAccountId);
+    }
+
+    public void notifyTransferReceived(Long toAccountId, Long fromAccountId,
+                                       BigDecimal amount, BigDecimal newBalance) {
+        Map<String, Object> payload = Map.of(
+                "type", "TRANSFER_RECEIVED",
+                "title", "Transfer Received",
+                "message", "You received $" + amount + " from account #" + fromAccountId,
+                "amount", amount.toString(),
+                "newBalance", newBalance.toString(),
+                "fromAccountId", fromAccountId,
+                "accountId", toAccountId,
+                "timestamp", LocalDateTime.now().toString()
+        );
+
+        sendToAccount(toAccountId, payload);
+        log.info("WebSocket: Transfer received notification to account {}", toAccountId);
+    }
 }
