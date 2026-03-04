904ffecd9f633cdbeda61101dbdd513cac6cc01d
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-03 21:31:58 -0600

    Modified WebSocketTestController.java

--- banking-app/src/main/java/com/banking/app/controller/WebSocketTestController.java
+++ banking-app/src/main/java/com/banking/app/controller/WebSocketTestController.java
@@ -23,4 +23,25 @@ public class WebSocketTestController {
         wsService.notifyDeposit(accountId, amount, new BigDecimal("10000"));
         return ResponseEntity.ok(Map.of("message", "Deposit notification sent to account " + accountId));
     }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Map<String, String>> testTransfer(@RequestBody Map<String, Object> request) {
+        Long fromId = Long.valueOf(request.get("fromAccountId").toString());
+        Long toId = Long.valueOf(request.get("toAccountId").toString());
+        BigDecimal amount = new BigDecimal(request.get("amount").toString());
+
+        wsService.notifyTransferSent(fromId, toId, amount, new BigDecimal("5000"));
+        wsService.notifyTransferReceived(toId, fromId, amount, new BigDecimal("8000"));
+        return ResponseEntity.ok(Map.of("message", "Transfer notifications sent"));
+    }
+
+    @PostMapping("/loan-approved")
+    public ResponseEntity<Map<String, String>> testLoanApproved(@RequestBody Map<String, Object> request) {
+        Long accountId = Long.valueOf(request.get("accountId").toString());
+        Long loanId = Long.valueOf(request.get("loanId").toString());
+        BigDecimal amount = new BigDecimal(request.get("amount").toString());
+
+        wsService.notifyLoanApproved(accountId, loanId, amount);
+        return ResponseEntity.ok(Map.of("message", "Loan approved notification sent"));
+    }
 }
