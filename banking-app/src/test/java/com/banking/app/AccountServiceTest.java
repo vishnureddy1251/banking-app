87404ee4128024629537296e4920cf516d80d234
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-04 23:31:15 -0600

    Added AccountServiceTest.java

---
+++ banking-app/src/test/java/com/banking/app/AccountServiceTest.java
@@ -0,0 +1,10 @@
+package com.banking.app;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("AccountService Tests")
+public class AccountServiceTest {
+}
