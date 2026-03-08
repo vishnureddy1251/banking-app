f126e2464072e4a1e7df3908ca1d5baf36d47fa0
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-08 16:27:00 -0500

    Modified BankingAppApplication.java

--- banking-app/src/main/java/com/banking/app/BankingAppApplication.java
+++ banking-app/src/main/java/com/banking/app/BankingAppApplication.java
@@ -2,8 +2,10 @@ package com.banking.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
