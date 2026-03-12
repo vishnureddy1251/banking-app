1872ab721c14a61e669e2f855031eeb8dda6e381
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-11 23:14:05 -0500

    Added request logging to LoanController.java

--- banking-app/src/main/java/com/banking/app/controller/LoanController.java
+++ banking-app/src/main/java/com/banking/app/controller/LoanController.java
@@ -4,6 +4,7 @@ import com.banking.app.model.Loan;
 import com.banking.app.service.LoanService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/loans")
 @RequiredArgsConstructor
@@ -47,6 +49,7 @@ public class LoanController {
 
     @PutMapping("/{id}/approve")
     public ResponseEntity<Loan> approveLoan(@PathVariable Long id) {
+        log.info("REST: Loan approval request for loan ID {}", id);
         return ResponseEntity.ok(loanService.approveLoan(id));
     }
 
