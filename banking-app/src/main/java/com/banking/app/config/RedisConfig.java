a49e9d9976ab4af250118313dfda89a2ea75eac8
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-15 11:19:36 -0500

    Updated RedisConfig.java

--- banking-app/src/main/java/com/banking/app/config/RedisConfig.java
+++ banking-app/src/main/java/com/banking/app/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.banking.app.config;
 
-import lombok.Value;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,7 +9,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Value("${app.redis.cache-ttl-minutes:5}")
