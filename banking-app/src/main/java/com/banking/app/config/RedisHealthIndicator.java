f8aa1567859588db0089e9af1a41f63668c91750
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-15 10:57:24 -0500

    Updated RedisHealthIndicator.java

--- banking-app/src/main/java/com/banking/app/config/RedisHealthIndicator.java
+++ banking-app/src/main/java/com/banking/app/config/RedisHealthIndicator.java
@@ -2,10 +2,19 @@ package com.banking.app.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class RedisHealthIndicator {
+public class RedisHealthIndicator implements HealthIndicator {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Override
+    public Health health() {
+        return null;
+    }
 }
